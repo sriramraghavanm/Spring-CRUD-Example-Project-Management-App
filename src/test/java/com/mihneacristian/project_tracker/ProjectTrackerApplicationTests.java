8ec277c28a10b205274599b9f6d086efd364819b
sriramraghavanm <raghavan.23@live.in>
2023-06-01 00:06:08 +0000

    Fix for build failure due to empty test

--- src/test/java/com/mihneacristian/project_tracker/ProjectTrackerApplicationTests.java
+++ src/test/java/com/mihneacristian/project_tracker/ProjectTrackerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.mihneacristian.project_tracker;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class ProjectTrackerApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext context) {
+	    assertThat(context).isNotNull();
     }
 
 }
